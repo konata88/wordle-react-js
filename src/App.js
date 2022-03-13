08476db03c4f790d5f6f3a117dabc1aeff26714c
roman <teplitsky@runnet.ru>
2022-03-13 23:47:59 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Letter from './components/Letter'
+import Board from './components/Board'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: this.getEmptyBoard(),
+      attempt: 0,
+      maxAttempts: 5,
+      word: '',
+      wordlist: [],
+      win: false,
+      warning: '',
+      usedLetters: []
+    };
+  }
+  componentWillMount() {
+    fetch("/words.txt")
+        .then(res => res.json())
+        .then(
+            (result) => {
+              const randWord = result[Math.floor((Math.random() * result.length))];
+              this.setState({...this.state, wordlist: result, word: randWord});
+            });
+  }
+
+  getEmptyBoard = () => {
+    return [0,1,2,3,4,5].map(row => {
+      const letters = [];
+      [0,1,2,3,4].map(row => {
+        letters.push({letter:'', color:''})
+      });
+      return letters;
+    })
+  }
+
+  cantPlay = () => {
+    return (this.state.win || (this.state.attempt > 5));
+  }
+
+  selectLetter = (name) => {
+    if (this.cantPlay()) return;
+    const newboard = this.state.board;
+    const emptyLetter = newboard[this.state.attempt].find(l => l.letter === '');
+    if (emptyLetter) {
+      emptyLetter.letter = name;
+    }
+    this.setState({...this.state, board: newboard});
+  }
+
+  backLetter = (name) => {
+    if (this.cantPlay()) return;
+
+    const newboard = this.state.board;
+    const filled = newboard[this.state.attempt].filter(l => l.letter !== '');
+    if (filled.length) {
+      filled[filled.length-1].letter = '';
+    }
+    this.setState({...this.state, board: newboard});
+  }
+
+  newGame = () => {
+    const randWord = this.state.wordlist[Math.floor((Math.random() * this.state.wordlist.length))];
+    this.setState({...this.state, word: randWord, attempt: 0, win: false, warning: '', usedLetters: [],  board: this.getEmptyBoard()});
+  }
+
+  completeWord = () => {
+    let warning = '';
+    if (this.cantPlay()) return;
+
+    const newboard = this.state.board;
+    const usedLetters = this.state.usedLetters;
+    const currentBoard = newboard[this.state.attempt];
+    const emptyLetter = currentBoard.filter(l => l.letter === '');
+    if (emptyLetter.length) {
+      return;
+    }
+    const word = currentBoard.map(l => l.letter).join('').toLowerCase();
+    const exist = this.state.wordlist.find(w => w === word)
+    if (!exist) {
+      warning = `${word}  - this word does not exist`;
+    } else {
+      word.split('').forEach((letter, index) => {
+        if (letter === this.state.word[index]) {
+          currentBoard[index].color = 'green';
+        } else {
+          const words = this.state.word.split('');
+          const otherPlace = words.find(w => w === letter)
+          currentBoard[index].color = (otherPlace) ? 'yellow' : 'grey';
+          if (!otherPlace) {
+            usedLetters.push(letter);
+          }
+        }
+      })
+    }
+    const win = word === this.state.word;
+    const attempt = exist ? this.state.attempt + 1 : this.state.attempt ;
+
+    if(attempt > 5) {
+      warning = `You lost. Word: ${this.state.word.toUpperCase()}`;
+    }
+    if(win) {
+      warning = 'You won!';
+    }
+    this.setState({...this.state, board: newboard, attempt: attempt, win: win, warning: warning, usedLetters: usedLetters});
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <h2>Wordle</h2>
+          <h3>{this.state.warning}</h3>
+          {(this.cantPlay()) && <Letter name={'New game'} onClickFunc={this.newGame} usedLetters={[]}/>}
+          {[0,1,2,3,4,5].map(row => <Board letters={this.state.board[row]}/>)}
+          <div>
+            <div>
+              {['A','B','C','D','E','F','G','H','I'].map(l =><Letter name={l} onClickFunc={this.selectLetter} usedLetters={this.state.usedLetters}/>)}
+            </div>
+            <div>
+              {['J','K','L','M','N','O','P','Q','R'].map(l =><Letter name={l} onClickFunc={this.selectLetter} usedLetters={this.state.usedLetters}/>)}
+            </div>
+            <div>
+              {['S','T','U','V','W','X','Y','Z'].map(l =><Letter name={l} onClickFunc={this.selectLetter} usedLetters={this.state.usedLetters}/>)}
+            </div>
+            <div>
+              <Letter name={'back'} onClickFunc={this.backLetter} usedLetters={[]}/>
+              <Letter name={'complete'} onClickFunc={this.completeWord} usedLetters={[]}/>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
